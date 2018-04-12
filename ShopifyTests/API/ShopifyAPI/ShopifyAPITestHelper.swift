4447ef996b8d673d50340985b72636a440771171
Radyslav Krechet <radyslavkrechet@gmail.com>
2018-04-12 16:12:11 +0300

    Add article api tests

--- ShopifyTests/API/ShopifyAPI/ShopifyAPITestHelper.swift
+++ ShopifyTests/API/ShopifyAPI/ShopifyAPITestHelper.swift
@@ -23,9 +23,52 @@ struct ShopifyAPITestHelper {
                 "refundPolicy": shopPolicy,
                 "termsOfService": shopPolicy]
     }
-}
-
-class TestTask: Task {
-    func resume() {}
-    func cancel() {}
+    
+    static var author: [String: Any] {
+        return ["firstName": "First",
+                "lastName": "Last",
+                "name": "Name",
+                "email": "user@mail.com",
+                "bio": "Bio"]
+    }
+    
+    static var tags: [String] {
+        return ["First tag",
+                "Second tag"]
+        
+    }
+    
+    static var blog: [String: Any] {
+        return ["id": "BlogIdentifier",
+                "title": "Title"]
+    }
+    
+    static var image: [String: Any] {
+        return ["id": "ImageIdentifier",
+                "src": "Source",
+                "altText": "Text"]
+    }
+    
+    static var article: [String: Any] {
+        return ["id": "ArticleIdentifier",
+                "title": "Title",
+                "content": "Content",
+                "contentHtml": "Html",
+                "author": author,
+                "tags": tags,
+                "blog": blog,
+                "publishedAt": "2018-01-01T12:15:25+01:00",
+                "url": "www.google.com",
+                "image": image,
+                "__typename": "Article"]
+    }
+    
+    static var articleEdges: [[String: Any]] {
+        return [["node": article,
+                 "cursor": "Cursor"]]
+    }
+    
+    static var articles: [String: Any] {
+        return ["edges": articleEdges]
+    }
 }
